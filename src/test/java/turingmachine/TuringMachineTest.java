cf7e3786e12548504dd917cea4eb6d9f31565273
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-27 12:42:03 +0100

    Added unit tests

--- src/test/java/turingmachine/TuringMachineTest.java
+++ src/test/java/turingmachine/TuringMachineTest.java
@@ -1,7 +1,34 @@
 package turingmachine;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+
 /**
  * @author 150023118
  */
 public class TuringMachineTest {
+    TuringMachine tm;
+
+    @Before
+    public void setUp() {
+        File file = new File("./input.txt");
+        tm = new TuringMachine(file);
+    }
+
+    @Test
+    public void testValidInput() {
+        assertTrue(tm.processInput("aabb"));
+    }
+
+    @Test
+    public void testInvalidInput() {
+        assertFalse(tm.processInput("abbb"));
+    }
+
 }
