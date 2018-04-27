243791343f943ddebc7248138408e9636cc206fc
Ryosuke Minami <ryosukem13@gmail.com>
2018-04-27 15:26:18 +0100

    Added exception for transition conflicts in state

--- src/main/java/exceptions/TransitionConflictException.java
+++ src/main/java/exceptions/TransitionConflictException.java
@@ -3,5 +3,16 @@ package exceptions;
 /**
  * @author 150023118
  */
-public class TransitionConflictException {
+public class TransitionConflictException extends RuntimeException {
+
+    private String message;
+
+    public TransitionConflictException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
 }
