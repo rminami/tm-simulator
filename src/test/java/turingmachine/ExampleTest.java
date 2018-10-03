13ba1969e7de8ddba1e6b9161ed7829e315b7ddb
Ryosuke Minami <ryosukem13@gmail.com>
2018-10-03 13:03:15 +0100

    Added unit tests

---
+++ src/test/java/turingmachine/ExampleTest.java
@@ -0,0 +1,71 @@
+package turingmachine;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Ryosuke Minami
+ */
+public class ExampleTest {
+    private TuringMachine tm;
+
+    @Before
+    public void setUp() {
+        TuringFactory factory = new TuringFactory();
+        tm = factory.tmFromFilePath("tms/example.txt");
+    }
+
+    @Test
+    public void testValidInput1() {
+        assertTrue(tm.accepts("aabb"));
+    }
+
+    @Test
+    public void testValidInput2() {
+        assertTrue(tm.accepts("a"));
+    }
+
+    @Test
+    public void testValidInput3() {
+        assertTrue(tm.accepts("aaaaaa"));
+    }
+
+    @Test
+    public void testValidInput4() {
+        assertTrue(tm.accepts("bbbb"));
+    }
+
+    @Test
+    public void testValidInput5() {
+        assertTrue(tm.accepts("aaabb"));
+    }
+
+    @Test
+    public void testInvalidInput1() {
+        assertFalse(tm.accepts("bba"));
+    }
+
+    @Test
+    public void testInvalidInput2() {
+        assertFalse(tm.accepts("baba"));
+    }
+
+    @Test
+    public void testInvalidInput3() {
+        assertFalse(tm.accepts("abba"));
+    }
+
+    @Test
+    public void testInvalidInput4() {
+        assertFalse(tm.accepts("abab"));
+    }
+
+    @Test
+    public void testNotInAlphabet() {
+        assertFalse(tm.accepts("c"));
+    }
+
+}
